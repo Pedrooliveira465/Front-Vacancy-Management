ed7466a795ffbacb45960fb3daa7d6ab8bfb608e
Pedro Oliveira <pedro86gugalegal@gmail.com>
2024-04-12 08:24:40 -0300

    Creating vacancy registration

---
+++ src/main/java/br/com/pedrooliveira/front_vacancy_management/modules/company/dto/CreateJobsDTO.java
@@ -0,0 +1,11 @@
+package br.com.pedrooliveira.front_vacancy_management.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobsDTO {
+
+    private String description;
+    private String benefits;
+    private String level;
+}
